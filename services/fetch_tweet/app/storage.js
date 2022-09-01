578f8e0afd446a5d60652daf84934f2ad86a531b
Prashanth Subrahmanyam <ksprashanth@google.com>
2022-09-01 18:22:37 +0530

    Helper functions to save docs in GCS

---
+++ services/fetch_tweet/app/storage.js
@@ -0,0 +1,60 @@
+// Copyright 2022 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+
+const TWEET_FILE_PREFIX = 'tweets';
+const USER_FILE_PREFIX = 'users';
+
+/**
+* Save the list of tweets in a storage bucket
+* @param {*} tweets 
+*/
+async function saveTweets(tweets) {
+    console.log('Saving tweets');
+    try {
+        let filename = [TWEET_FILE_PREFIX, Date.now().toString()].join('-');
+        filename += '.json';
+        let bucketname = [process.env['GOOGLE_CLOUD_PROJECT'], TWEET_FILE_PREFIX].join('_');
+        let data = JSON.stringify(tweets);
+        await storage.bucket(bucketname).file(filename).save(data);
+        console.log('Tweets saved');
+    } catch (error) {
+        console.log(`Save failed with error - ${error.message}`);
+    }
+}
+
+/**
+* Save the list of users in a storage bucket
+* @param {*} users 
+*/
+async function saveUsers(users) {
+    console.log('Saving users');
+    try {
+        let filename = [USER_FILE_PREFIX, Date.now().toString()].join('-');
+        filename += '.json';
+        let bucketname = [process.env['GOOGLE_CLOUD_PROJECT'], USER_FILE_PREFIX].join('_');
+        let data = JSON.stringify(users);
+        await storage.bucket(bucketname).file(filename).save(data);
+        console.log('Users saved');
+    }  catch (error) {
+        console.log(`Save failed with error - ${error.message}`);
+    }  
+}
+
+module.exports = {
+    saveTweets: saveTweets,
+    saveUsers: saveUsers
+};
