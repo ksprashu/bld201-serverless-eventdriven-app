ef847fbca4c08e732c182a0c4a13bab4a16ec66a
Prashanth Subrahmanyam <ksprashanth@google.com>
2022-09-18 14:57:00 +0530

    Add CORS headers

--- functions/api-round-attempts/python/main.py
+++ functions/api-round-attempts/python/main.py
@@ -31,7 +31,25 @@ def http_receiver(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>
     """
-    return jsonify(fetch_per_round_best_attempt())
+
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }    
+    return (jsonify(fetch_per_round_best_attempt()), 200, headers)
 
 
 def fetch_per_round_best_attempt():
